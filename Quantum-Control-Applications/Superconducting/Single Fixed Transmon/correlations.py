6200118a2ded76c8807da4d4ec261a0c180693c5
niv drucker <niv@quantum-machines.co>
2022-06-07 18:53:50 +0300

    correlation is now working for multiple elements

--- Quantum-Control-Applications/Superconducting/Single Fixed Transmon/correlations.py
+++ Quantum-Control-Applications/Superconducting/Single Fixed Transmon/correlations.py
@@ -4,15 +4,24 @@ from configuration import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-tmax = 300  # clock cycles
-dt = 1  # clock cycles
-tvec = np.arange(0, tmax, dt)
-sig1 = 'out1'
-sig2 = 'out1'
 
-config['elements']['resonator_aux'] = config['elements']['resonator']
-config['elements']['resonator_aux']['operations']['zero'] = 'zero_pulse'
-config['integration_weights']['cosine_weights']['cosine'] = [(10000.0, tmax * 4)]
+# ==== parameters ==== #
+M = 4  # number of auxiliary elements
+tmax = 480 // 4;dt = 1; tvec = np.arange(0, tmax, dt)  # clock cycles
+print(len(tvec)/M)  # we need len(tvec)%M==0 if we don't want to deal with edge issues
+sig1 = 'out1'; sig2 = 'out2'  # between which outputs of the resonator to perform the correlation measurement
+readout_pules_length = 2000
+tof = 248
+
+
+# ==== editing the configuration ==== #
+config['pulses']['readout_pulse']['length'] = readout_pules_length
+config['elements']['resonator']['time_of_flight'] = tof
+for i in range(M):
+    config['elements'][f'resonator_aux_{i+1}'] = config['elements']['resonator']
+    config['elements'][f'resonator_aux_{i+1}']['operations']['zero'] = 'zero_pulse'
+
+config['integration_weights']['cosine_weights']['cosine'] = [(10, tmax * 4)]
 config['integration_weights']['cosine_weights']['sine'] = [(0.0, tmax * 4)]
 config['pulses']['zero_pulse'] = {'operation': 'measurement',
                                   'length': 16,
@@ -20,39 +29,59 @@ config['pulses']['zero_pulse'] = {'operation': 'measurement',
                                   'integration_weights': {'cos': 'cosine_weights'},
                                   'digital_marker': 'ON'}
 
+
+# ==== the correlation program ===== #
 with program() as correlations:
 
     n = declare(int)
+    m = declare(int)
     t = declare(int)
-    vec = [declare(fixed, size=len(tvec)) for i in range(2)]
+    vec = [declare(fixed, size=len(tvec)) for i in range(1+M)]
     corr_st = declare_stream()
-    corr_ = declare(fixed)
+    corr_ = [declare(fixed) for i in range(M)]
+    corr_vec = declare(fixed, size=len(tvec))
+    idx = declare(int)
 
-    with for_(n, 0, n < 100, n + 1):
-        with for_(t, 0, t < len(tvec), t + dt):
+    update_frequency("resonator", int(121e6))
+    with for_(m, 0, m < 2, m+1):
+        with for_(n, 0, n < 100, n + 1):
+            with for_(t, 0, t < len(tvec), t + dt*M):
 
-            wait(4, "resonator")
-            measure("readout", "resonator", None, integration.sliced("cos", vec[0], dt, sig1))
-            wait(4 + t, "resonator_aux")
-            measure("zero", "resonator_aux", None, integration.sliced("cos", vec[1], dt, sig2))
-            assign(corr_, Math.dot(vec[0], vec[1]))
-            save(corr_, corr_st)
+                # state preparation
+                wait(1000)
+                align()
+
+                # sliced measurements
+                wait(4, "resonator")
+                measure("readout", "resonator", None, integration.sliced("cos", vec[0], dt, sig1))
+                for i in range(M):
+                    wait(4 + t + dt * i, f"resonator_aux_{i+1}")
+                    measure("zero", f"resonator_aux_{i+1}", None, integration.sliced("cos", vec[1+i], dt, sig2))
+                    assign(corr_[i], Math.dot(vec[i+1], vec[0]))
+                    assign(corr_vec[idx], corr_vec[idx] + corr_[i])
+                    assign(idx, idx+1)
+            assign(idx, 0)
+
+        with for_(idx, 0, idx < len(tvec), idx+1):
+            save(corr_vec[idx], corr_st)
+            assign(corr_vec[idx], 0.0)
 
     with stream_processing():
         corr_st.buffer(len(tvec)).average().save('corr')
 
 
-#####################################
-#  Open Communication with the QOP  #
-#####################################
+# ==== execution and analysis ==== #
+qop_ip = "172.16.2.103"
+qop_port = 80
 qmm = QuantumMachinesManager(host=qop_ip, port=qop_port)
-
 qm = qmm.open_qm(config)
-
 job = qm.execute(correlations)
 res_handles = job.result_handles
 corr_handle = res_handles.get("corr")
 res_handles.wait_for_all_values()
 corr = corr_handle.fetch_all()
 t = tvec * 4  # transform to ns
-plt.plot(t, corr)
+
+plt.plot(t, corr, '-*')
+plt.xlabel('time in [ns]')
+plt.ylabel('correlation [a.u]')
